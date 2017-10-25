ae2b5698d8cf2b469ee63495c3c77fa065b704cd
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2017-10-25 10:31:51 +0300

    add todos

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <meta name="theme-color" content="#000000">
+    <meta name="theme-color" content="#000000"> <!-- @TODO: set theme color -->
     <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
