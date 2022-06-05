af92872a02304e6760ee48c9dc58a047ed4e4c09
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-06-05 23:16:57 +0300

    rm composer.json

--- functions.php
+++ functions.php
@@ -2,9 +2,5 @@
 
 const TEXT_DOMAIN = '';
 
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-    require_once __DIR__ . '/vendor/autoload.php';
-}
-
 $loader = new Innocode\WPThemeModule\Loader();
 $loader->run();
