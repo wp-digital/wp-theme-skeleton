1dd0a9e51f713b8b040e5639a49862739ddf59c4
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-07-28 17:39:45 +0300

    show friednly error for dev when no module loader installed

--- functions.php
+++ functions.php
@@ -2,5 +2,9 @@
 
 const TEXT_DOMAIN = ''; // @TODO: set text domain.
 
+if ( ! class_exists( 'Innocode\WPThemeModule\Loader' ) ) {
+	wp_die( 'Please install <a href="https://github.com/innocode-digital/wp-theme-module" target="_blank" rel="noopener noreferrer">Theme Module</a> library.' );
+}
+
 $loader = new Innocode\WPThemeModule\Loader();
 $loader->run();
